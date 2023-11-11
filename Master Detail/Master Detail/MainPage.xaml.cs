cf6a651b1cf06a95e839cefb971b6613b6a8a445
Fedi Beltaief <fediblt3@gmail.com>
2023-11-11 11:46:28 +0100

    Initial commit

---
+++ Master Detail/Master Detail/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Master_Detail
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void GoToPage1(Object sender,EventArgs e)
+        {
+            Navigation.PushAsync(new P1());
+        }
+        public void GoToPage2(Object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new P2());
+        }
+        public void GoToPage3(Object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new P3());
+        }
+    }
+}
---
+++ Master Detail/Master Detail/MasterFlyoutPageFlyout.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Master_Detail
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterFlyoutPageFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public MasterFlyoutPageFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new MasterFlyoutPageFlyoutViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MasterFlyoutPageFlyoutViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MasterFlyoutPageFlyoutMenuItem> MenuItems { get; set; }
+
+            public MasterFlyoutPageFlyoutViewModel()
+            {
+                MenuItems = new ObservableCollection<MasterFlyoutPageFlyoutMenuItem>(new[]
+                {
+                    new MasterFlyoutPageFlyoutMenuItem { Id = 0, Title = "Page 1", TargetType=typeof(P1) },
+                    new MasterFlyoutPageFlyoutMenuItem { Id = 1, Title = "Page 2", TargetType=typeof(P2) },
+                    new MasterFlyoutPageFlyoutMenuItem { Id = 2, Title = "Page 3", TargetType=typeof(P3) }
+                }) ; ; ;
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
